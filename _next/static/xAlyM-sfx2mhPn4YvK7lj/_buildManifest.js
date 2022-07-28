06c31858d84c2919b5e98204b01136735ad27c99
hongmin78 <hongminsn@gmail.com>
2022-07-28 20:03:10 +0900

    Updates

---
+++ _next/static/xAlyM-sfx2mhPn4YvK7lj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-ee08b6aa6dec9f4d034a.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
